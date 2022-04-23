41f6821ea71bab3657469bc12127e7839410014d
Pedro Yvo Lucas <91639878+pedrokaa2@users.noreply.github.com>
2022-04-23 02:26:18 -0300

    feat: Routes folder

---
+++ src/Routes/index.jsx
@@ -0,0 +1,21 @@
+import { Switch, Route } from "react-router";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path = "/">
+        
+      </Route>
+
+      <Route exact path = "/register">
+
+      </Route>
+
+      <Route exact path = "/home">
+
+      </Route>
+    </Switch>
+  )
+}
+
+export default Routes
