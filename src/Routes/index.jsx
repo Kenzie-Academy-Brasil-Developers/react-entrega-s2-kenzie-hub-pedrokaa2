83bf86925224328ff26ae04b72c2b6b0cf26c735
Pedro Yvo Lucas <91639878+pedrokaa2@users.noreply.github.com>
2022-04-23 02:44:38 -0300

    fix: routes

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "react-router"
+import { Switch, Route } from "react-router-dom"
 import Home from '../Pages/Home'
 import Login from '../Pages/Login'
 import Register from '../Pages/Register'
